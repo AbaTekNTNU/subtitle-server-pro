d7eb04f1e50e2caa54ab699be9783d8eabe4f577
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2025-03-12 17:01:45 +0100

    Fix simultanious subs

--- src/app/_components/subtitleView.tsx
+++ src/app/_components/subtitleView.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import useCurrentSubtitle from "~/app/_hooks/useCurrentSubtitle";
 import type { Subtitle } from "~/server/api/routers/subs";
 
 type Props = {
-  initialSubtitle?: Subtitle ;
-}
+  initialSubtitle?: Subtitle;
+};
 
 export default function SubtitleView({ initialSubtitle }: Props) {
   const currentSubtitle = useCurrentSubtitle(initialSubtitle);
@@ -15,8 +15,19 @@ export default function SubtitleView({ initialSubtitle }: Props) {
   }
 
   return (
-    <div className="flex flex-col text-stroke-black w-full items-center justify-end h-screen mt-auto left-0 right-0 bg-none text-white text-center text-[52px] p-2">
-      {currentSubtitle.bottom}
-    </div>
+    <>
+      <div className="text-stroke-black left-0 right-0 mt-auto flex h-screen w-full flex-col items-center justify-end bg-none p-2 text-center text-[52px] text-white">
+        <p className="absolute bottom-0 left-0 right-0">
+          {currentSubtitle.bottom}
+        </p>
+        <p className="absolute left-0 right-0 top-0">{currentSubtitle.top}</p>
+        <p className="absolute bottom-1/2 left-0 translate-y-1/2">
+          {currentSubtitle.left}
+        </p>
+        <p className="absolute bottom-1/2 right-0 translate-y-1/2">
+          {currentSubtitle.right}
+        </p>
+      </div>
+    </>
   );
 }
