01e862421127149d0f8739ed52ae2573329ecb0a
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2025-03-13 15:19:23 +0100

    Reduce line-height on subs view

--- src/app/_components/subtitleView.tsx
+++ src/app/_components/subtitleView.tsx
@@ -17,21 +17,23 @@ export default function SubtitleView({ initialSubtitle }: Props) {
   return (
     <>
       <div className="text-stroke-black left-0 right-0 mt-auto flex h-screen w-full flex-col items-center justify-end bg-none p-2 text-center text-[52px] text-white">
-        <p className="absolute bottom-0 left-0 right-0">
+        <p className="absolute bottom-0 left-0 right-0 leading-none">
           {currentSubtitle.bottom}
         </p>
-        <p className="absolute left-0 right-0 top-0">{currentSubtitle.top}</p>
+        <p className="absolute left-0 right-0 top-0 leading-none">
+          {currentSubtitle.top}
+        </p>
         {currentSubtitle.left && currentSubtitle.right ? (
           <>
-            <p className="absolute bottom-1/2 left-0 translate-y-1/2">
+            <p className="absolute bottom-1/2 left-0 translate-y-1/2 leading-none">
               {currentSubtitle.left}
             </p>
-            <p className="absolute bottom-1/2 right-0 translate-y-1/2">
+            <p className="absolute bottom-1/2 right-0 translate-y-1/2 leading-none">
               {currentSubtitle.right}
             </p>
           </>
         ) : (
-          <p className="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2">
+          <p className="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 leading-none">
             {currentSubtitle.right ?? currentSubtitle.left}
           </p>
         )}
